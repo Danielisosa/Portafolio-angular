b524353a71420ae9ceaab11906362514d8659df3
Danielisosa <danielisosa.0512@gmail.com>
2022-05-13 10:48:22 -0400

    listos los productos en firebase y se muestran en la pagina de portafolio

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,14 +8,29 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     //leer el archivo Json
     this.http
       .get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
+      });
+  }
+  private cargarEquipo() {
+    //leer el archivo Json
+    this.http
+      .get('https://angular-html-6ada8-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe((resp: any) => {
+        this.equipo = resp;
       });
   }
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductosService {
+  cargando = true;
+  productos: Producto[] = [];
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos() {
+    this.http
+      .get(
+        'https://angular-html-6ada8-default-rtdb.firebaseio.com/productos_idx.json'
+      )
+      .subscribe((resp: any) => {
+        console.log(resp);
+        this.productos = resp;
+
+        setTimeout(() => {
+          this.cargando = false;
+        }, 1000);
+      });
+  }
+}
+//https://angular-html-6ada8-default-rtdb.firebaseio.com/productos_idx.jso
